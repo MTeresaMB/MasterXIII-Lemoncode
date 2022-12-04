fc78b9cc49fc7302bfda5d8a4a810036e3418849
MTeresaMB <milanesteresa@gmail.com>
2022-12-04 20:04:41 +0100

    UPDATE

--- Modulo-2-Lenguajes/Laboratorio/src/00 javascript-fundamentals/exercise.ts
+++ Modulo-2-Lenguajes/Laboratorio/src/00 javascript-fundamentals/exercise.ts
@@ -1,3 +1,3 @@
-console.log("************** PRACTICE *********************");
+console.log("****** EXCERCISES JAVASCRIPT FUNDAMENTALS ******");
 console.log("Use folder 00 practice to practice with homework exercises");
 console.log("You can add new files as long as they are imported from index.ts");
