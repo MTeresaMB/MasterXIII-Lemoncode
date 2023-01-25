b8cfacbfbb87b960a64755142e2bf0b94fad6512
MTeresaMB <milanesteresa@gmail.com>
2023-01-25 21:27:28 +0100

    update laboratorio

---
+++ Modulo-3-Bundling/wp-typescript/src/index.js
@@ -0,0 +1,6 @@
+import "./styles.scss";
+import logoImg from "./content/logo_2.png";
+
+const img = document.createElement("img");
+img.src = logoImg;
+document.getElementById("imgContainer").appendChild(img);
---
+++ Modulo-3-Bundling/wp-typescript/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: "./index.js",
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename: "index.html",
+      scriptLoading: "blocking",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+}
