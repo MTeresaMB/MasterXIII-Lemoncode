890257eb723d278fa0264de1dc3fb56e66f0213f
MTeresaMB <milanesteresa@gmail.com>
2023-02-02 19:53:09 +0100

    finished webpack parcel

--- Modulo-3-Bundling/wp-parcel/src/hello.component.tsx
+++ Modulo-3-Bundling/wp-parcel/src/hello.component.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './content/logo_1.png';
+import * as classes from './styles.module.scss'
 
-export const Hello: React.FC = () => {
+export const Hello: React.FC = () =>{
   return (
-    <div>
-      <h2>Hello from Hello Component</h2>
-      <img src={logo} alt="logo_1"/>
+    <div className={classes.container}>
+      <div className={classes.text}>
+        <h2 >Hello Lemoncoders from React</h2>
+      </div>
+      <img src={logo} className={classes.logo} alt="logo_1"/>
     </div>
   )
 }
