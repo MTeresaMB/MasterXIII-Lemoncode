a15e8d85c966a2e3bf8db6b989c3dd76bfc5964e
MTeresaMB <milanesteresa@gmail.com>
2023-02-07 14:21:37 +0100

    modified styles

--- Modulo-3-Bundling/webpack-project/src/index.tsx
+++ Modulo-3-Bundling/webpack-project/src/index.tsx
@@ -5,8 +5,10 @@ import classes from "./index.scss";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <div>
-    <h1>Hello World from React DOM</h1>
-    <p>This is a simple react app</p>
+  <div className={classes.container}>
+    <div className={classes.title}>
+      <h1>Hello World from React DOM</h1>
+    </div>
+    <p className={classes.paragraph}>This is a simple react app</p>
   </div>
 );
