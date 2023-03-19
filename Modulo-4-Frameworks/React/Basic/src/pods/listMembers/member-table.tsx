baed087f890ad019be3c913d4d162e6c4620c5b9
MTeresaMB <milanesteresa@gmail.com>
2023-03-19 21:36:30 +0100

    update pagination

--- Modulo-4-Frameworks/React/Basic/src/pods/listMembers/member-table.tsx
+++ Modulo-4-Frameworks/React/Basic/src/pods/listMembers/member-table.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import { MemberEntity } from "@/model/MemberEntity";
 import { MemberTableRow } from "./member-table-row";
 import { OrganizationSearch } from "./organization-search";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import classes from './list-style.css';
 import { OrganizationContext } from "@/app";
+import ReactPaginate from "react-paginate";
 
 export const MemberTable = () => {
   const {organizationName} = React.useContext(OrganizationContext);
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [pageNumber, setPageNumber] = React.useState(0);
+
+  const membersPerPage = 6;
+  const indexOfLastMember = pageNumber * membersPerPage;
+
+  const displayMembers = members
+  .slice(indexOfLastMember, indexOfLastMember + membersPerPage)
+  .map((member) => {
+    return (
+      <TableRow key={member.id}>
+        <MemberTableRow key={member.id} member={member}/>
+      </TableRow>
+    )
+  });
 
   React.useEffect(() => {
     const handleSearch = () => {
@@ -39,12 +54,27 @@ export const MemberTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {members.map((member) => (
-              <TableRow key={member.id}>
-                <MemberTableRow key={member.id} member={member}/>
-              </TableRow>
-            ))}
+            {displayMembers}  
           </TableBody>
+          <Box>
+            <ReactPaginate
+              previousLabel={"Previous"}
+              nextLabel={"Next"}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={Math.ceil(members.length / membersPerPage)}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={6}
+              onPageChange={({ selected }) => {
+                setPageNumber(selected);
+              }}
+              containerClassName={classes.paginationBttns}
+              previousLinkClassName={classes.previousBttn}
+              nextLinkClassName={classes.nextBttn}
+              disabledClassName={classes.paginationDisabled}
+              activeClassName={classes.paginationActive}
+            ></ReactPaginate>        
+          </Box>
           </Table>
         </TableContainer>
     </>
