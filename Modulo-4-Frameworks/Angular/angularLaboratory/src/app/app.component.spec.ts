039a2e1bd27181c8dadcd715d321e19a8b4d5f41
MTeresaMB <milanesteresa@gmail.com>
2023-03-24 21:42:08 +0100

    create angular project

--- Modulo-4-Frameworks/Angular/angularLaboratory/src/app/app.component.spec.ts
+++ Modulo-4-Frameworks/Angular/angularLaboratory/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'moduleAngularLaboratory'`, () => {
+  it(`should have as title 'angularLaboratory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('moduleAngularLaboratory');
+    expect(app.title).toEqual('angularLaboratory');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('moduleAngularLaboratory app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularLaboratory app is running!');
   });
 });
