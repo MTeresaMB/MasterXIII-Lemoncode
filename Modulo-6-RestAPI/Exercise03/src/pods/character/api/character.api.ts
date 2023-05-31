f6453c70761dbdc2f633d12c77fa2870d5712d79
MTeresaMB <milanesteresa@gmail.com>
2023-05-31 11:57:40 +0200

    change getCharacter of character.api.ts

--- Modulo-6-RestAPI/Exercise03/src/pods/character/api/character.api.ts
+++ Modulo-6-RestAPI/Exercise03/src/pods/character/api/character.api.ts
@@ -1,9 +1,30 @@
 import { Character } from './character.api-model';
-import axios from 'axios';
+import { gql } from 'graphql-request';
+import { graphQLClient } from '../../../core/api/graphql-client';
+
+interface CharacterRequest {
+  character: Character;
+}
 
 export const getCharacter = async (id: string): Promise<Character> => {
-  const url = `https://rickandmortyapi.com/api/character/${id}`;
-  const {data} = await axios.get(url);
-  return data
+  const query = gql`
+    query{
+      character(id: "${id}"){
+        id
+        name
+        status
+        species
+        type
+        gender
+        origin{name}
+        location{name}
+        image
+        episode{episode}
+        created
+      }
+    }
+  `
+  const { character } = await graphQLClient.request<CharacterRequest>(query);
+  return character;
 };
 
